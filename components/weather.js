b7c3a0769f05bde9ebd76164c3eba1295f4bdf5a
hunter britten <huntergbritten@gmail.com>
2021-04-24 11:49:28 -0700

    refactoring

--- components/weather.js
+++ components/weather.js
@@ -1,25 +1,46 @@
 'use strict'
 
 const superagent = require('superagent');
+// const notFound = require('./not-found');
 require('dotenv').config();
 
-async function getWeatherHandler(request, response) {
+const inMemoryDB = {};
 
+async function getWeatherHandler(request, response) {
   const lat = request.query.lat;
   const lon = request.query.lon;
+  try {
 
-  const key = process.env.WEATHER_API_KEY;
+    const weatherAlreadyFound = inMemoryDB[lat + lon] !== undefined;
 
-  const url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${key}`
-  const weatherResponse = await superagent.get(url);
+    if (weatherAlreadyFound && (Date.now() - inMemoryDB[lat + lon].timestamp < 3600000)) {
+      const forecasts = inMemoryDB[lat + lon].forecasts;
+      // console.log('success', inMemoryDB)
+      response.status(200).send(forecasts);
 
-  const weatherObject = JSON.parse(weatherResponse.text);
+    } else {
 
-  const weatherArray = weatherObject.data;
+      const key = process.env.WEATHER_API_KEY;
 
-  const forecasts = weatherArray.map(day => new WeatherDay(day));
+      const url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${key}`
 
-  response.send(forecasts);
+      const weatherResponse = await superagent.get(url);
+
+      const weatherObject = JSON.parse(weatherResponse.text);
+
+      const weatherArray = weatherObject.data;
+
+      const forecasts = weatherArray.map(day => new WeatherDay(day));
+      inMemoryDB[lat + lon] = { forecasts, timestamp: Date.now() };
+      // inMemoryDB[lat + lon] = {forecasts, dateCreated: is time stamp value older than ___? get new data and overwrite old memoryDB};\
+      console.log('reset weather');
+      response.status(200).send(forecasts);
+    }
+  } catch (err) {
+    console.error('error', err);
+    response.status(500).send('error', err);
+  }
+  // console.log(lat + lon);
 }
 
 class WeatherDay {
